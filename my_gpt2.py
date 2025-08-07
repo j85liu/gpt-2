1542ee50101c902fe0faa436c8c17613e40fee5f
James Liu <james.liu@columbia.edu>
2025-08-07 14:27:38 -0400

    Update my_gpt2.py

--- my_gpt2.py
+++ my_gpt2.py
@@ -6,3 +6,12 @@ from dataclasses import dataclass
 import torch
 import torch.nn as nn
 from torch.nn import functional as F
+
+@dataclass
+class GPTConfig:
+    block_size: int = 256
+    vocab_size: int = 65
+    n_layer: int = 6
+    n_head: int = 6
+    n_embd: int = 384
+
