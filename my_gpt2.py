0093ab8687a33296def20ac36829c102c536c78f
James Liu <james.liu@columbia.edu>
2025-08-23 09:52:57 -0400

    Update my_gpt2.py

--- my_gpt2.py
+++ my_gpt2.py
@@ -49,3 +49,4 @@ class GPT(nn.Module):
     def from_pretrained(cls, model_type):
         """Loads pretrained GPT-2 model weights from huggingface"""
         assert model_type in {'gpt2', 'gpt2-medium', 'gpt2-large', 'gpt2-xl'}
+        from transformers import GPT2LMHeadModel
