2828b56f0e390daa40dd785f9e723436654f7f1f
James Liu <james.liu@columbia.edu>
2025-09-02 15:04:43 -0400

    Update my_gpt2.py

--- my_gpt2.py
+++ my_gpt2.py
@@ -91,6 +91,6 @@ model.to('cpu')
 import tiktoken
 enc = tiktoken.get_encoding('gpt2')
 tokens = enc.encode("Hello, I'm a language model,")
-tokens = torch.tensor(tokens, dtype=torch.long)
-tokens = tokens.unsqueeze(0), repeat(num_return_sequences, 1)
+tokens = torch.tensor(tokens, dtype=torch.long) # (8, )
+tokens = tokens.unsqueeze(0), repeat(num_return_sequences, 1) # (5, 8)
 x = tokens.to('cpu')
