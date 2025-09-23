27b1cd8df954ce93fb087f70a300f6f446f3dad6
James Liu <james.liu@columbia.edu>
2025-09-23 08:30:56 -0400

    Update my_gpt2.py

--- my_gpt2.py
+++ my_gpt2.py
@@ -51,7 +51,12 @@ class GPT(nn.Module):
         self.apply(self._init_weights)
 
     def _init_weights_(self.module):
-        
+        if isinstance(module, nn.Linear):
+            torch.nn.init.normal_(module.weight, mean=0.0, std=0.02)
+            if module.bias is not None:
+                torch.nn.init.zeros_(module.bias)
+        elif isinstance(module, nn.Embedding):
+            torch.nn.init.normal_(module.weight, mean=0.0, std=0.02)
 
     def forward(self, idx, targets=None):
         # idx is of shape (B, T)
