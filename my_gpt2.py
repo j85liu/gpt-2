a686f3c1068445fb844a0c9145b840f11a23a93b
James Liu <james.liu@columbia.edu>
2025-09-24 17:18:57 -0400

    Update my_gpt2.py

--- my_gpt2.py
+++ my_gpt2.py
@@ -12,7 +12,10 @@ class MLP(nn.Module):
     def __init__(self, config):
         super().__init__()
         self.c_fc    = nn.Linear(config.n_embd, 4* config.n_embd)
-        
+        self.gelu    = nn.GELU(approximate='tanh')
+        self.c_proj  = nn.Linear(4 * config.n_embd, config.n_embd)
+
+
     pass
 
 class Block(nn.Module):
