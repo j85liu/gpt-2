d03783b6487a59eda2e427b47b90f4af25ae6cfa
James Liu <james.liu@columbia.edu>
2025-10-15 12:26:19 -0400

    Update my_gpt2.py

--- my_gpt2.py
+++ my_gpt2.py
@@ -38,6 +38,10 @@ class CausalSelfAttention(nn.Module):
         y = self.c_proj(y)
         return y
 
+class TanhGELU(nn.Module):
+    def forward(self, input):
+        return 0.5 * input * (1.0 + torch.tanh(math.sqrt(2.0 / math.pi) * (input + 0.044715 * torch.pow(input, 3.0))))
+
 class MLP(nn.Module):
 
     def __init__(self, config):
