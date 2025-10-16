1b159e7380f6e171608d661cd39b523aed950bdc
James Liu <james.liu@columbia.edu>
2025-10-16 08:44:15 -0400

    Update my_gpt2.py

--- my_gpt2.py
+++ my_gpt2.py
@@ -218,7 +218,7 @@ train_loader = DataLoaderLite(B=4, T=32)
 torch.set_float32_matmul_precision('high')
 
 # get logits
-model = GPT(GPTConfig())
+model = GPT(GPTConfig(vocab_size=50304))
 model.to(device)
 model = torch.compile(model)
 
