eb559590426b9b700ac25c4891d31a44e1e6ac7e
James Liu <james.liu@columbia.edu>
2025-10-20 09:41:18 -0400

    Update my_gpt2.py

--- my_gpt2.py
+++ my_gpt2.py
@@ -254,7 +254,7 @@ def get_lr(it):
 
 
 # optimize
-optimizer = torch.optim.AdamW(model.parameters(), lr=3e-4)
+optimizer = torch.optim.AdamW(model.parameters(), lr=3e-4, betas=(0.9, 0.95), eps=1e-8)
 for i in range(50):
     t0 = time.time()
     x, y = train_loader.next_batch()
