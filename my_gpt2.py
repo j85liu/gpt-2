a46e1e721f21691bd91748705b7f86a3b7e46b31
James Liu <james.liu@columbia.edu>
2025-10-26 13:02:56 -0400

    Update my_gpt2.py

--- my_gpt2.py
+++ my_gpt2.py
@@ -185,6 +185,7 @@ class GPT(nn.Module):
         num_decay_params = sum(p.numel() for p in decay_params)
         num_nodecay_params = sum(p.numel() for p in nodecay_params)
         print(f"num decayed parameter tensors: {len(decay_params)}, with {num_decay_params:,} parameters")
+        print(f"num non-decayed parameter tensors: {len(nodecay_params)}, with {num_nodecay_params:,} parameters")
 
 # --------------------------------------------------------
 import tiktoken
