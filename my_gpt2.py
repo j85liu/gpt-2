bcd70bdd3eda3c946ac4b8f9c7f027d256ad5847
James Liu <james.liu@columbia.edu>
2025-10-29 08:51:03 -0400

    Update my_gpt2.py

--- my_gpt2.py
+++ my_gpt2.py
@@ -244,7 +244,7 @@ total_batch_size = 524288 # 2**19 ~ 0.5M, in number of tokens
 B = 16 # micro batch size
 T = 1024 # sequence length
 assert total_batch_size % (B * T) == 0, "make sure total_batch_size is divisible by B * T"
-
+grad_accum_steps = total_batch_size // (B * T)
 
 train_loader = DataLoaderLite(B, T)
 
