f3bbc93ad11ba2cb0056fddd9196188f59f44004
James Liu <james.liu@columbia.edu>
2025-11-11 10:04:15 -0500

    Update my_gpt2.py

--- my_gpt2.py
+++ my_gpt2.py
@@ -236,6 +236,7 @@ if ddp:
     ddp_rank = int(os.environ['RANK'])
     ddp_local_rank = int(os.environ['LOCAL_RANK'])
     ddp_world_size = int(os.environ['WORLD_SIZE'])
+    device = f'cuda:{ddp_local_rank}'
 else:
     pass
 
