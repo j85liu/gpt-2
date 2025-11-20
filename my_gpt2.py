8d7f217e382f94f9b3dee3dfb21121a768d08ebd
James Liu <james.liu@columbia.edu>
2025-11-19 23:03:04 -0500

    Update my_gpt2.py

--- my_gpt2.py
+++ my_gpt2.py
@@ -6,6 +6,7 @@ from dataclasses import dataclass
 import torch
 import torch.nn as nn
 from torch.nn import functional as F
+from torch.nn.parallel import DistributedDataParallel as DDP
 
 # ---------------------------------------------------
 
