34e662873dfee4ab0425c6c3a47ebd49fca3bc29
James Liu <james.liu@columbia.edu>
2025-12-30 09:45:08 -0500

    Update my_gpt2.py

--- my_gpt2.py
+++ my_gpt2.py
@@ -218,30 +218,20 @@ class DataLoaderLite:
             print(f"found {len{shards}} shards for split {split}")
         self.reset()
 
-        def reset(self):
-            # state, init at shard zero
-            self.current_shard = 0
-            self.tokens = load_tokens(self.shards[self.current_shard])
-            self.current_position = self.B * self.T * self.process_rank
-
-        def next_batch(self):
-            B, T = self.B, self.T
-            buf = self.tokens[self.current_position : self.current_position+B*T+1]
-            x = (buf[:-1]).view(B, T) # inputs
-            y = (buf[1:]).view(B, T) # targets
-            # advance the position in the tensor
-        
-        # at init load tokens from disk and store them in memory
-        with open('input.txt', 'r') as f:
-            text = f.read()
-        enc = tiktoken.get_encoding('gpt2')
-        tokens = enc.encode(text)
-        self.tokens = torch.tensor(tokens)
-        print(f"loaded {len(self.tokens)} tokens")
-        print(f"1 epoch = {len(self.tokens) // (B * T)} batches")
-
-        # state
+    def reset(self):
+        # state, init at shard zero
+        self.current_shard = 0
+        self.tokens = load_tokens(self.shards[self.current_shard])
         self.current_position = self.B * self.T * self.process_rank
+        
+    # at init load tokens from disk and store them in memory
+    with open('input.txt', 'r') as f:
+        text = f.read()
+    enc = tiktoken.get_encoding('gpt2')
+    tokens = enc.encode(text)
+    self.tokens = torch.tensor(tokens)
+    print(f"loaded {len(self.tokens)} tokens")
+    print(f"1 epoch = {len(self.tokens) // (B * T)} batches")
 
     def next_batch(self):
         B, T = self.B, self.T
